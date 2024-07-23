2f19a26ec77585ff25e3b5a682128d24f011d2cc
sergei_yanenco <yanencos@gmail.com>
2024-07-23 23:05:02 +0300

    init_folse

--- src_1/tests.py
+++ src_1/tests.py
@@ -12,28 +12,10 @@ cart = [
 ]
 
 
-def get_mask_card_number(card_number: str) -> str:
-    """Маскировки номера карты"""
-
-    if len(card_number) != 16:
-        return "Не верный формат ввода."
-
-    masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-    return masked_number
-
-#перенести в модуль waidget.py
-def get_mask_account(number_account: str) -> str:
-    """Маскировки номера счета"""
-    for number in number_account:
-        if number.isdigit():
-            cel_number = f"{'*' * len(number_account[:2]) + number_account[2:]}"
-            return cel_number
-        else:
-            return "Не верный формат ввода"
-
-
 # исправить принемает только один аргумент строку, содержащию тип и номер карты или счёта
 # accaunt=None, check=None
+
+
 def mask_account_card(check: str) -> str:
     """Маскировки номера счета"""
     account_list = []
@@ -43,7 +25,7 @@ def mask_account_card(check: str) -> str:
         for i in check:
             masked = i[:5] + "**" + (i[8:])
             account_list.append(masked)
-
+        return account_list
     # if accaunt:
     #     for accoun in accaunt:
     #         masked = "**" + accoun[2:]
@@ -51,11 +33,5 @@ def mask_account_card(check: str) -> str:
     # account_list.extend(account_list_not_name)
     # return f"{account_list}"
 
-
-number_account = "19899891283123"
-print(get_mask_account(number_account))
-
-card_number = "7000792289606361"
-print(get_mask_card_number(card_number))
 
 print(mask_account_card(cart))
